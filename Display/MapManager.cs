acfc193aa39d8676a6d6294cc77f307610197a45
SyouKawa <chernoar_syoukawa_bisyoufish@outlook.com>
2018-08-07 09:44:20 +0800

    try to create Panel-frame-demo

--- Display/MapManager.cs
+++ Display/MapManager.cs
@@ -7,11 +7,11 @@ using System.Threading;
 
 namespace Game_VSmode_verTest{
     class MapManager {
-        const int row=25;
-        const int column=activemap_col+datapanel_col;
-        const int datapanel_col=30;
-        const int activemap_col=60;
-        const int bagpanel_col=30;
+        const int row=13;
+        const int column=activemap_col+datapanel_col+20;
+        const int datapanel_col=15;
+        const int activemap_col=30;
+        const int bagpanel_col=15;
         int player_pos_x;
         int player_pos_y;
         
@@ -25,23 +25,35 @@ namespace Game_VSmode_verTest{
 
         StringBuilder mapBuffer=new StringBuilder();
 
+        public MapManager(){
+            NewGameMap();
+        }
+
+        public void NewGameMap(){
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                    gameMap[i,j]=new Point();
+                }
+            }
+        }
+
         public void GetPlayerCurPos(){
             char cur_key=Console.ReadKey(true).KeyChar;
             switch(cur_key){//TO-DO check the limit condition.
                 case 'w':
-                    if(player_pos_x>1&&gameMap[player_pos_x-1,player_pos_y]==' ')
+                    if(player_pos_x>1&&gameMap[player_pos_x-1,player_pos_y].display=="  ")
                         player_pos_x-=1;
                 break;
                 case 'a':
-                    if(player_pos_y>1&&gameMap[player_pos_x,player_pos_y-1]==' ')
+                    if(player_pos_y>1&&gameMap[player_pos_x,player_pos_y-1].display=="  ")
                         player_pos_y-=1;
                 break;
                 case 's':
-                    if(player_pos_x<59&&gameMap[player_pos_x+1,player_pos_y]==' ')
+                    if(player_pos_x<59&&gameMap[player_pos_x+1,player_pos_y].display=="  ")
                         player_pos_x+=1;
                 break;
                 case 'd':
-                    if(player_pos_y<59&&gameMap[player_pos_x,player_pos_y+1]==' ')
+                    if(player_pos_y<59&&gameMap[player_pos_x,player_pos_y+1].display=="  ")
                         player_pos_y+=1;
                 break;
             }
@@ -52,13 +64,14 @@ namespace Game_VSmode_verTest{
             InitPlayer();
             while(true){
                 GetPlayerCurPos();
-                DrawAllPanel();
+                //DrawAllPanel();
+                DrawDataPanel();
             }
         }
         public void InitGameMap(){
             for(int i=0;i<row;i++){
                 for(int j=0;j<column;j++){
-                    gameMap[i,j]=' ';
+                    gameMap[i,j].display="  ";
                 }
             }
         }
@@ -68,84 +81,132 @@ namespace Game_VSmode_verTest{
             string temp_ref=new string('>',count);
             return temp_ref;
         }
-        public void SetAString(int x_index,int y_index,char[] str){
-            for(int i=0;i<str.Length;i++){
-                gameMap[y_index,x_index+i]=str[i];
+        public void SetAString(int x_index,int y_index,string str){
+            gameMap[y_index,x_index].display=str;
+            int temp_len=0;
+            if(str.Length/2!=0) temp_len+=1;
+            for(int i=y_index+1;i<=temp_len+2;i++){//
+                gameMap[i,x_index].display="";
             }
         }
-
         public void InitMapPanel(){
-            for(int i=0;i<activemap_col;i++) gameMap[0,i]='-';
+            for(int i=0;i<activemap_col;i++) gameMap[0,i].display="一";
             for (int lr_limit = 1; lr_limit < row; lr_limit++) {
-                gameMap[lr_limit, 0] = '|';
-                gameMap[lr_limit, activemap_col - 1] = '|';
+                gameMap[lr_limit, 0].display = "| ";
+                gameMap[lr_limit, activemap_col - 1].display = "| ";
             }
-            for (int i=0;i<activemap_col;i++) gameMap[row-1,i]='-';
+            for (int i=0;i<activemap_col;i++) gameMap[row-1,i].display="一";
             
         }
         public void InitTargetBox(){
             int pos_x=seed.Next(1,row);
             int pos_y=seed.Next(1,activemap_col);
-            gameMap[pos_x,pos_y]='o';
+            gameMap[pos_x,pos_y].display="★";
         }
         public void InitPlayer(){
             player_pos_x=seed.Next(1,row);
             player_pos_y=seed.Next(1,activemap_col);
-            gameMap[player_pos_x,player_pos_y]='v';
+            gameMap[player_pos_x,player_pos_y].display="♀";
         }
-        //char[,] dataPanel=new char[,]{"Name:Alan".ToCharArray(),{},{},{},{}}
-        public void InitDataPanel(){
-            int x_start=activemap_col+1;
-            int panel_row=10;
-            int panel_col=28;
-            //content-set
-            SetAString(x_start+1,1,"Name:Alan".ToCharArray());
-            SetAString(x_start+1,2,("HP:"+ConvertNum2Pic(90)).ToCharArray());
-            SetAString(x_start+1,3,("MP:"+ConvertNum2Pic(70)).ToCharArray());
-            SetAString(x_start+1,4,"Fatigue:0 Fear:0 Anger:0".ToCharArray());
-            SetAString(x_start+1,5,"Skills:".ToCharArray());
-            //bound-set
-            for(int i=x_start;i<panel_col+x_start;i++) gameMap[0,i]='-';
-            for (int lr_limit = 1; lr_limit < panel_row; lr_limit++) {
-                gameMap[lr_limit, x_start] = '|';
-                gameMap[lr_limit, x_start + panel_col - 1] = '|';
+        
+        public void DrawDataPanel(){
+            int x_start=activemap_col+1;//30+1
+            int panel_row=8;//set to const in class
+            int panel_col=30;
+            List<string>showdata=new List<string>();
+            showdata.Add("Name:Alan");
+            showdata.Add("Name:Alan");//in DataPanel index begin from 1
+            showdata.Add("HP:"+ConvertNum2Pic(90));
+            showdata.Add("MP:"+ConvertNum2Pic(70));
+            showdata.Add("Fatigue:0 Fear:0 Anger:0");
+            showdata.Add("Skills:");
+            for(int p_line=0;p_line<panel_row;p_line++){
+                Console.SetCursorPosition(x_start,p_line);
+                for(int i=panel_col;(p_line==0||p_line==panel_row-1)&&i>0;i-=2){
+                    Console.Write("一");//the-first-line and the-last-line
+                }
+                if(p_line!=0){
+                    Console.Write("|");
+                    if(p_line<showdata.Count){//still exist content
+                        Console.Write(showdata[p_line]);
+                        string left_space=new string(' ',panel_col-showdata[p_line].Length-1);
+                        Console.Write(left_space);
+                        Console.Write("|");
+                    }else if(p_line!=panel_row-1){//Content over but not the last-line
+                        string left_space=new string(' ',panel_col-1);
+                        Console.Write(left_space);
+                        Console.Write("|");
+                    }
+                }
             }
-            for(int i=x_start;i<panel_col+x_start;i++) gameMap[panel_row-1,i]='-';
         }
-
         public void InitBagPanel(){
             int x_start=activemap_col+1;
-            int y_start=10;
-            int panel_row=10;
-            int panel_col=28;
+            int y_start=6;
+            int panel_row=6;
+            int panel_col=14;
             //content-set
-            SetAString(x_start+1,panel_row+1,"    Bag".ToCharArray());
-            SetAString(x_start+panel_col/2+1,panel_row+1,"    Box".ToCharArray());
+            SetAString(x_start+1,panel_row+1,"    Bag");
+            SetAString(x_start+panel_col/2+1,panel_row+1,"    Box");
             //bound-set
-            for(int i=x_start;i<x_start+panel_col;i++) gameMap[y_start,i]='-';
+            for(int i=x_start;i<x_start+panel_col;i++) gameMap[y_start,i].display="一";
             for (int i = y_start+1; i < y_start + panel_row; i++) {
-                gameMap[i, x_start] = '|';
-                gameMap[i,x_start+panel_col/2]='|';
-                gameMap[i, x_start + panel_col - 1] = '|';
+                gameMap[i, x_start].display= "| ";
+                gameMap[i,x_start+panel_col/2].display="| ";
+                gameMap[i, x_start + panel_col - 1].display = "| ";
             }
-            for (int i=x_start;i<x_start+panel_col;i++) gameMap[y_start+panel_row,i]='-';
+            for (int i=x_start;i<x_start+panel_col;i++) gameMap[y_start+panel_row,i].display="一";
         }
+        public void DrawBagPanel(string title){//TODO-Create a Class to save the former 4-args 
+            int x_start=activemap_col+1;
+            int y_start=8;
+            int panel_row=10;
+            int panel_col=21;
+            List<string>showdata=new List<string>();//read from JsonFile
+            showdata.Add("Ice Storm");
+            showdata.Add("Shadow of Gloom");
+            showdata.Add("Hit with MagicWand");
+            showdata.Add("Curse of BloodMoon");
+            Console.SetCursorPosition(x_start,y_start+1);//last panel +1 line to start 
+            int simple_space_count=(panel_col-title.Length)/2/2;
+            string temp_space1=new string('-',simple_space_count);
+            string temp_space2=new string(' ',simple_space_count);
+            Console.Write(temp_space1+temp_space2+title+temp_space2+temp_space1);
 
+            for(int p_line=y_start+2,data_line=0;p_line<panel_row+y_start;p_line++,data_line++){
+                Console.SetCursorPosition(x_start,p_line);
+                if(p_line!=y_start+panel_row-1) Console.Write("|");//except the last line
+                if(data_line<showdata.Count){//still exist content
+                    Console.Write(showdata[data_line]);
+                    string left_space=new string(' ',panel_col-showdata[data_line].Length-1);
+                    Console.Write(left_space);
+                    Console.Write("|");
+                }else if(p_line!=y_start+panel_row-1){//Content over but not the last-line
+                    string left_space=new string(' ',panel_col-1);
+                    Console.Write(left_space);
+                    Console.Write("|");
+                }else if(p_line==y_start+panel_row-1){
+                    for(int i=panel_col;i>0;i--){
+                        Console.Write("-");//the-last-line
+                    }
+                }
+            }
+        }
         public void DrawAllPanel(){
             mapBuffer.Clear();
             //Console.Clear();
             
             InitGameMap();
             InitMapPanel();
-            gameMap[player_pos_x,player_pos_y]='v';
+            gameMap[player_pos_x,player_pos_y].display="♀";
             //SConsole.SetCursorPosition
-            //gameMap[10,10]='■';
-             /*TODO-if(OpenState())*/InitDataPanel();
-            /*TODO-if(OpenBag())*/InitBagPanel();
+            gameMap[10,10].display="■";
+             /*TODO-if(OpenState())*/ //InitDataPanel();
+            /*TODO-if(OpenBag())*/ InitBagPanel();
             
             for(int i=0;i<row;i++){
                 for(int j=0;j<column;j++){
-                    mapBuffer.Append(gameMap[i,j]);
+                    mapBuffer.Append(gameMap[i,j].display);
                 }
                 mapBuffer.Append("\n");
             }
