378515290f779f0b13f68b3d18d96f2a926ad4d2
SyouKawa <chernoar_syoukawa_bisyoufish@outlook.com>
2018-08-22 15:26:45 +0800

    Spawn RandomRoomsMap with Colliders in Unity

---
+++ RougeMap/ColliderMethod/RoomManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RoomManager:MonoBehaviour{
+	
+	#region Singleton
+	private RoomManager _instance;
+	public RoomManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				GameObject.Find("RoomManager").GetComponent<RoomManager>();
+			}
+			return _instance;
+		}
+	}
+	#endregion
+
+	string loadPath;
+	public List<GameObject> bottom { get; private set; }
+	public List<GameObject> top { get; private set; }
+	public List<GameObject> left { get; private set; }
+	public List<GameObject> right { get; private set; }
+
+	private void Start()
+	{
+		//default
+		loadPath = "Assets/Resources/Prefabs/";
+		bottom = new List<GameObject>();
+		top = new List<GameObject>();
+		left = new List<GameObject>();
+		right = new List<GameObject>();
+		InitRoomArray();
+	}
+
+	private void InitRoomArray()
+	{
+		if (Directory.Exists(loadPath))
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(loadPath);
+			FileInfo[] files = dirInfo.GetFiles("*" , SearchOption.AllDirectories);
+
+			foreach (FileInfo curFile in files)
+			{
+				if (curFile.Name.EndsWith(".prefab"))
+				{
+					//Debug.Log(curFile.Name);
+					string loadName = "Prefabs/" + curFile.Name.Split('.')[0];
+					if (curFile.Name.Contains("T"))
+					{
+						top.Add(Resources.Load<GameObject>(loadName));
+					}
+					if (curFile.Name.Contains("B"))
+					{
+						bottom.Add(Resources.Load<GameObject>(loadName));
+					}
+					if (curFile.Name.Contains("L"))
+					{
+						left.Add(Resources.Load<GameObject>(loadName));
+					}
+					if (curFile.Name.Contains("R"))
+					{
+						right.Add(Resources.Load<GameObject>(loadName));
+					}
+				}
+			}
+
+			//TestCode
+			foreach (GameObject room in bottom)
+				Debug.Log(room.name);
+		}
+	}
+}
---
+++ RougeMap/ColliderMethod/RoomSpawner.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour {
+	public int openingDirection;
+	//1->need bottom door
+	//2->need top door
+	//3->need left door
+	//4->need right door
+	private RoomManager manager;
+	private Random random;
+	public bool isSpawned;
+
+	private void Start()
+	{
+		isSpawned = false;
+		random = new Random();
+		manager = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomManager>();
+		Invoke("Spawn" , 0.1f);
+	}
+	private void TestFuncDelay() { }
+
+	private void Spawn()
+	{
+		if (!isSpawned)
+		{
+			int index = -1;//save random index.
+			switch (openingDirection)
+			{
+				case 1://need bottom-door
+					index = Random.Range(0 , manager.bottom.Count);
+					Instantiate(manager.bottom[index] , transform.position , manager.bottom[index].transform.rotation);
+					break;
+				case 2://need top-door
+					index = Random.Range(0 , manager.top.Count);
+					Instantiate(manager.top[index] , transform.position , manager.top[index].transform.rotation);
+					break;
+				case 3://need left-door
+					index = Random.Range(0 , manager.left.Count);
+					Instantiate(manager.left[index] , transform.position , manager.left [index].transform.rotation);
+					break;
+				case 4://need right-door
+					index = Random.Range(0 , manager.right .Count);
+					Instantiate(manager.right [index] , transform.position , manager.right [index].transform.rotation);
+					break;
+			}
+		}
+		isSpawned = true;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("SpawnPoint"))
+		{
+			if (collision.GetComponent<RoomSpawner>().isSpawned == true && isSpawned == false)
+			{//face a already spawned.
+				isSpawned = true;
+				//Destroy(gameObject);
+				//Destroy(collision.gameObject);
+			}
+			if (collision.GetComponent<RoomSpawner>().isSpawned == false && isSpawned == false)
+			{
+				//Spawn();
+				isSpawned = true;
+			}
+		}
+	}
+}
